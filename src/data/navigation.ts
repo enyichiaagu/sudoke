53ffc20d1504861ebb7364783ce1da0f9baa9ccd
A. Enyichi Agu <enyichiaagu@gmail.com>
2026-05-14 13:21:28 +0100

    style: made homepage look better

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const navLinks: { name: string; href: string }[] = [
+	{ name: "Solutions", href: "/#solutions" },
+	{ name: "Projects", href: "/#projects" },
+	{ name: "Testimonials", href: "/#testimonials" },
+	{ name: "Contact Us", href: "/#contact" },
+];
